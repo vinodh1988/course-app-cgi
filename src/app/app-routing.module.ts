60d7976d1cdb0a3e0a33fe725d1be1d5a307fad2
vinodh1988 <k.c.vinodh@hotmail.com>
2019-01-30 14:18:09 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { ReviewsComponent } from './routes/reviews/reviews.component';
+import { ContactComponent } from './routes/contact/contact.component';
+import { VirtualclassComponent } from './routes/virtualclass/virtualclass.component';
+import { OffersComponent } from './routes/offers/offers.component';
+import { NewcoursesComponent } from './routes/newcourses/newcourses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"reviews",component:ReviewsComponent},
+  {path:"contacts",component:ContactComponent},
+  {path:"vclasses",component:VirtualclassComponent},
+  {path:"offers",component:OffersComponent},
+  {path:"newcourses",component:NewcoursesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { HomeComponent } from './routes/home/home.component';
+import { ReviewsComponent } from './routes/reviews/reviews.component';
+import { NewcoursesComponent } from './routes/newcourses/newcourses.component';
+import { VirtualclassComponent } from './routes/virtualclass/virtualclass.component';
+import { ContactComponent } from './routes/contact/contact.component';
+import { OffersComponent } from './routes/offers/offers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
+    HomeComponent,
+    ReviewsComponent,
+    NewcoursesComponent,
+    VirtualclassComponent,
+    ContactComponent,
+    OffersComponent
   ],
   imports: [
     BrowserModule,
