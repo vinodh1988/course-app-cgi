cefa5b68751240e9ca80fcfb2356cf71bb07a11f
vinodh1988 <k.c.vinodh@hotmail.com>
2019-01-31 15:38:52 +0530

    latest commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
 
+
+interface Token{
+  access_token:string;
+  refresh_token:string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+username:string;
+password:string;
+  constructor(private login:LoginService) { }
 
   ngOnInit() {
   }
+
+  loginActivity(){
+    //let dataobj={username:this.username,password:this.password,grant_type:"password"};
+    let data="username="+this.username+"&"+
+    "password="+this.password+"&grant_type=password";
+    this.login.logMethod(data).subscribe(
+      (data:Token)=>{
+        console.log(data);
+        localStorage.setItem('token',data.access_token);
+        alert("logged successfuly");
+        
+      },
+      ()=>{alert("failure")}
+      
+    )
+}
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+httpOptions;
+username:string;
+password:string;
+  constructor(private http:HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+        'Authorization': "Basic "+btoa("vinodh:password")
+      })
+   }
+  }
 
-  constructor() { }
+  logMethod(data):Observable<Object>{
+    //data object will contain username,password and granttype
+    console.log(this.httpOptions);
+  
+    return this.http.post("http://162.241.222.49:8896/authorize/jwt",data,this.httpOptions);
+  }
 }
